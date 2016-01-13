403b2215fba1781bdf1619610941af63b652da55
莴笋Ho <gzhehai@foxmail.com>
2016-01-13 11:45:54 +0800

    add object Copy Paste do support

---
+++ src/Aima.Extension/ObjectExtension4CopyPaste.cs
@@ -0,0 +1,50 @@
+﻿/***************************************************************
+*
+*   AimaTeam开源项目（版权所有：copyright@aimaTeam.com）       
+*   尽管它开源,我们真心希望您可以为我们保留的版权信息，谢谢
+----------------------------------------------------------------
+*   作   者：Hai he
+*   日   期：2015/12/8 1:37:34
+*   博   客：https://hehai.aimateam.com
+*   说   明：
+----------------------------------------------------------------
+*
+*   官方QQ群号：139849106
+*   官方  网站：https://www.aimateam.com
+****************************************************************/
+
+namespace Aima.Extension
+{
+    /// <summary>
+    /// Object类型常用操作而定义的扩展方法静态类
+    /// </summary>
+    public static partial class ObjectExtension4CopyPaste
+    {
+        /// <summary>
+        /// 复制一个对象
+        /// </summary>
+        /// <typeparam name="Ttarget"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Ttarget Copy<Ttarget>(this Ttarget source) where Ttarget : class, new()
+        {
+            var sp = new object();
+            var sp_New = sp.Paste();
+
+            throw new System.NotImplementedException();
+            // return default(Ttarget);
+        }
+
+        /// <summary>
+        /// 粘贴一个对象
+        /// </summary>
+        /// <typeparam name="Ttarget"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static Ttarget Paste<Ttarget>(this Ttarget source) where Ttarget : class, new()
+        {
+            throw new System.NotImplementedException();
+            // return default(Ttarget);
+        }
+    }
+}
